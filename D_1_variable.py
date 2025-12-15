96213421ce7ec81266f73b8c5a81a7de7c670d0a
Rajin-Saleh <rajinp752@gmail.com>
2025-12-16 00:12:48 +0600

    Variables of Python

---
+++ D_1_variable.py
@@ -0,0 +1,6 @@
+#Practise 1
+
+info_mail = 'info@darawithbaraa.com'
+support_mail = 'support@datawithbaraa.com'
+site = 'www.datawithbaraa.com'
+print(f"To learn this Python course visit {site}.\n\nIf you have any Query, email at {info_mail} and if you need support than mail at {support_mail}")
