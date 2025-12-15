da7d257796f68200210a39bc07d3f6efa9376a33
Rajin-Saleh <rajinp752@gmail.com>
2025-12-16 00:30:26 +0600

    Input/Output of Python

---
+++ D_1_input.py
@@ -0,0 +1,9 @@
+## Practise 2
+# Basic info collection
+
+name = input("Enter Your Name --> ")
+country = input("Enter your country --> ")
+age = int(input("Enter your age -->  "))
+
+
+print(f"My name is {name} and i am from {country}. I am {age} years old")
